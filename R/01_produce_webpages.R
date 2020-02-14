2ef97576b50717af1c19b9e396d8721c336468fb
Kristen Dybala <kdybala@gmail.com>
2020-02-14 14:45:58 -0800

    add stub for script to render Rmd

---
+++ R/01_produce_webpages.R
@@ -0,0 +1,32 @@
+
+# README ------------------------------------------------------------------
+
+# script to render Rmd files and update all web pages
+
+
+# PROJECT README ----------------------------------------------------------
+
+rmarkdown::render(here::here('Rmd/README.Rmd'), 
+                  output_file = here::here('README.md'))
+
+
+# LANDING PAGE ------------------------------------------------------------
+
+
+
+# HOW ARE THE BIRDS DOING -------------------------------------------------
+
+
+
+# WEATHER & CLIMATE -------------------------------------------------------
+
+
+
+# HABITAT & COMMUNITY CHANGE ----------------------------------------------
+
+
+
+# CROSSROADS & CONNECTIONS ------------------------------------------------
+
+
+
