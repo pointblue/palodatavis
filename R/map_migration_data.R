6ef8aef3453c55e3cebd27193f387971f62c1d17
Kristen Dybala <kdybala@gmail.com>
2020-03-23 16:36:03 -0700

    rename script to map migration data; update to include SWTH range map and popup data

--- R/map_migration_data.R
+++ R/map_migration_data.R
@@ -11,6 +11,8 @@ library(leaflet)
 coordpath <- "rawdat/Summary_MarinBirds_MeanLocsWhereTheyWent.xlsx"
 iconpath <- "images/icons/logo.png"
 
+swthrange <- 'rawdat/cath_ustu_pl.shp'
+
 # PALETTE
 pointblue.palette <- c('#4495d1', '#74b743', '#f7941d', '#005baa', '#bfd730',
                        '#a7a9ac', '#666666')
@@ -27,13 +29,25 @@ shp <- readxl::read_excel(here::here(coordpath)) %>%
   mutate(specieslab = recode(Species, 'HETH' = "Hermit Thrush",
                              'GCSP' = "Golden-crowned Sparrow",
                              'FOSP' = "Fox Sparrow",
-                             'SWTH' = "Swainson's Thrush"))
+                             'SWTH' = "Swainson's Thrush"),
+         TagType = recode(TagType, 'Light' = 'Geolocator'))
 
 pal1 <- colorFactor(palette = pointblue.palette[c(3,2,1,4)], 
                     domain = unique(shp$specieslab))
 
+pal2 <- colorFactor(palette = pointblue.palette[c(3,2)], 
+                    domain = unique(swthpoly$season))
+
 logoicon <- makeIcon(iconUrl = here::here(iconpath), 
                      iconWidth = 25, iconHeight = 25)
+
+swthpoly <- st_read(here::here(swthrange)) %>%
+  st_set_crs('+proj=longlat +datum=WGS84') %>%
+  filter(ORIGIN != 4) %>% #drop migration range
+  group_by(ORIGIN) %>%
+  summarize() %>%
+  mutate(season = case_when(ORIGIN == 2 ~ 'breeding',
+                            ORIGIN == 3 ~ 'wintering'))
 
 # CREATE MAP --------------------------------------------------------------
 
@@ -43,16 +57,35 @@ map1 <- leaflet(shp) %>%
   # background terrain
   addProviderTiles("Esri.WorldGrayCanvas") %>% 
   
+  # range map
+  addPolygons(data = swthpoly,
+              fillColor = ~ pal2(season),
+              stroke = FALSE,
+              group = "Swainson's Thrush") %>% 
+  
   # add icon for Palo
   addMarkers(lng = -122.735527, lat = 37.929851, 
              icon = logoicon,
-             label = "Palomarin Field Station") %>% 
+             label = "Palomarin Field Station",
+             popup = "Palomarin Field Station") %>% 
   
   # add migration data
-  addCircleMarkers(radius = 3,
+  addCircleMarkers(radius = 5,
                    stroke = FALSE,
                    fillColor =  ~ pal1(specieslab),
-                   fillOpacity = 1) %>% 
+                   fillOpacity = 0.7,
+                   label = ~TagType,
+                   popup = ~TagType) %>% 
+  
+  addLayersControl(overlayGroups = c('Fox Sparrow', 'Golden-crowned Sparrow',
+                                     'Hermit Thrush', "Swainson's Thrush"),
+                   position = "bottomleft",
+                   options = layersControlOptions(collapsed = TRUE)) %>%
+  hideGroup('Golden-crowned Sparrow') %>%
+  hideGroup('Hermit Thrush') %>%
+  hideGroup('Fox Sparrow') %>%
+  hideGroup("Swainson's Thrush") %>% 
+  
   addLegend(pal = pal1,
             values = shp$specieslab,
             title = 'Species',
