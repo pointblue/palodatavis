bc52ac7ba5a97e8f5d44ff761fca6ef3096c3466
Kristen Dybala <kdybala@gmail.com>
2020-03-26 16:18:54 -0700

    add calls to produce widgets for each page

--- R/01_produce_webpages.R
+++ R/01_produce_webpages.R
@@ -18,17 +18,27 @@ rmarkdown::render(here::here('Rmd/index.Rmd'),
 
 # HOW ARE THE BIRDS DOING -------------------------------------------------
 
+source('R/graph_annual_capturestats_total.R')
+
+source('R/graph_annual_capturestats_focalspp.R')
+
 rmarkdown::render(here::here('Rmd/bandingstats.Rmd'), 
                   output_file = here::here('docs/banding.html'))
 
 
 # WEATHER & CLIMATE -------------------------------------------------------
 
+source('R/graph_annual_weatherstats.R')
+
 rmarkdown::render(here::here('Rmd/climate.Rmd'), 
                   output_file = here::here('docs/climate.html'))
 
 
 # HABITAT & COMMUNITY CHANGE ----------------------------------------------
+
+source('R/graph_annual_vegheight.R')
+
+source('R/graph_annual_territories.R')
 
 rmarkdown::render(here::here('Rmd/habitat.Rmd'), 
                   output_file = here::here('docs/habitat.html'))
