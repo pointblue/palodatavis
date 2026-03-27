69a4bcf81d3cf4e9dace411fe44c3689526f9362
Kristen Dybala <kdybala@gmail.com>
2026-03-27 16:33:25 -0700

    update total capture stats through fall 2025

--- R/graph_annual_capturestats_total.R
+++ R/graph_annual_capturestats_total.R
@@ -7,93 +7,168 @@
 library(tidyverse)
 library(plotly)
 
-#INPUT DATA
-# original copies:
-# nethrspath <- 'Z:/Terrestrial/band/nethrs/allpalonethrs.dbf'
-# bandpath <- 'Z:/Terrestrial/programs_and_projects/palomarin/Palodata/Band/allbandpalo.dbf'
-
-# local copies:
-nethrspath <- "rawdat/allpalonthrs_through2018.CSV"
-bandpath <- "rawdat/allbandpalo_through2018.CSV"
+# INPUT DATA
+# path to local copies of net hours and banding data
+nethrspath <- "rawdat/allpalonthrs.dbf"
+bandpath <- "rawdat/allnumb.dbf"
 
 # PALETTE
 pointblue.palette <- c('#4495d1', '#74b743', '#f7941d', '#005baa', '#bfd730',
                        '#a7a9ac', '#666666')
 
-#OUTPUT
+# OUTPUT
+# file path of final interactive graphic widget
 out <- "docs/widget/graph_total_capturestats.html"
 
 # CALCULATE STATS ---------------------------------------------------------
 
-# total net hours per year:
-effort <- read_csv(here::here(nethrspath), 
-                   col_types = cols_only(location = col_factor(), 
-                                         date = col_date(format = '%m/%d/%Y'), 
-                                         nethours = col_number())) %>% 
-  filter(location == 'PN') %>% 
-  mutate(year = format(date, '%Y'),
-         month = format(date, '%m')) %>% 
-  filter(year >= 1979) %>% 
-  # total by year and month:
-  group_by(year, month) %>% 
-  summarize(nethours = sum(nethours)) %>% 
-  ungroup()
+## NET HOURS------
+# net hours database: (drop extra columns labeled "X")
+effort_raw <- foreign::read.dbf(nethrspath) |> select(PROJECT:DUPE)
+str(effort_raw)
 
+# filter to PN after 1979
+unique(effort_raw$LOCATION) # PN, PGUP, MUHO, PIGU, LACR, RECR, G5, CT, PT, HUMP, PEXA, PALO
+summary(effort_raw$DATE) # 1976-01-03 through 2025-12-31
+
+effort = effort_raw |> 
+  filter(LOCATION == 'PN') |> 
+  filter(DATE >= '1979-01-01')
+  
+# summarize by year and month
+effort_sum = effort |> 
+  mutate(year = format(DATE, '%Y'),
+         month = format(DATE, '%m')) |> 
+  group_by(year, month) |> 
+  summarize(nethours = sum(NETHOURS),
+            .groups = 'drop')
+# 564 obs of 3 vars
+
+rm(effort_raw, effort)
+
+## CAPTURE STATS--------
 # total captures per year
-captures <- read_csv(here::here(bandpath), 
-                     col_types = cols_only(loc = 'f',
-                                           date = col_date(format = '%m/%d/%Y'),
-                                           size = 'f',
-                                           code = 'f',
-                                           bandnumb = 'n',
-                                           spec = 'f',
-                                           age = 'n',
-                                           sex = 'f')) %>% 
-  mutate(year = format(date, '%Y'),
-         month = format(date, '%m')) %>% 
-  filter(loc == 'PN' & year >= 1979) %>% 
-  # count total captures for each year and month
-  group_by(year, month) %>% 
-  count() %>% 
-  ungroup()
 
-# calculate capture rate per 1000 net hours, and fit smoothed line
-dat <- left_join(captures, effort, by = c('year', 'month')) %>%
+band_raw = foreign::read.dbf(bandpath) # slow because this is a large database!
+str(band_raw)
+
+# filter to PN after 1979 and no later than last net hours
+# > NOTE: not limited to new captures
+unique(band_raw$LOC)
+summary(band_raw$DATE) # 1966-01-14 thruogh 2026-03-26 (and 1 NA)
+
+band = band_raw |> select(INITIALS:COM) |> 
+  filter(LOC == 'PN') |> 
+  filter(DATE >= '1979-01-01') |> 
+  filter(DATE <= max(effort$DATE))
+# 151173 obs of 35 vars
+
+# summarize number of captures by year and month
+captures_sum = band |> 
+  mutate(year = format(DATE, '%Y'),
+         month = format(DATE, '%m')) |> 
+  group_by(year, month) |> 
+  count() |> 
+  ungroup()
+# 564 obs of 3 vars
+
+testthat::test_that('test that net hours and captures match', {
+  testthat::expect_true(nrow(effort_sum) == nrow(captures_sum))
+  testthat::expect_true(all(unique(effort_sum$year) %in% captures_sum$year))
+  testthat::expect_true(all(unique(captures_sum$year) %in% effort_sum$year))
+})
+
+## JOIN-----------
+# join captures and net hours, and assign months to specific seasons
+dat = full_join(captures_sum, effort_sum, by = c('year', 'month')) |> 
   mutate(year = as.numeric(year),
          month = as.numeric(month),
-         season = case_when(month >= 3 & month <= 7 ~ 'spring',
-                            month >= 8 & month <= 10 ~ 'fall',
-                            TRUE ~ 'winter'),
+         season = case_when(month >= 3 & month <= 7 ~ 'spring', # 5 months
+                            month >= 8 & month <= 10 ~ 'fall', # 3 months
+                            TRUE ~ 'winter'), # 4 months
          # group Jan and Feb with previous Nov and Dec
          year = case_when(month <= 2 ~ year - 1,
-                          TRUE ~ year)) %>% 
-  group_by(year, season) %>% 
+                          TRUE ~ year))
+
+testthat::test_that('test that there are no mismatched months', {
+  testthat::expect_false(any(is.na(dat$n)))
+  testthat::expect_false(any(is.na(dat$nethours)))
+})
+
+summary(dat)
+# n: mean = 268; 1st Q = 112; min = 24
+# nethours: mean = 2396; 1st Q = 1561; min = 480
+
+hist(dat$n) # long tail with few  months > 1000
+dat |> filter(n > 1000) |> arrange(desc(n)) 
+# 7 months with > 1000 captures: 
+# >> 2 in October (also the two highest): 1981, 1980
+# >> 5 in July (in descending order): 1985, 1980, 1982, 1979
+# (all of these months had > 3000 net hours)
+
+hist(dat$nethours, breaks = 16) # bimodal (b/c seasonal) with few months < 1000
+dat |> filter(nethours < 1000)
+# 3 months with < 1000 net hours: 12/1980; 01/1995; 09/2025
+
+# check for complete years:
+dat |> group_by(year) |> count() |> filter(n < 12)
+# >> 1978 (because Jan/Feb 1979)
+# >> 2025 (10 months because Jan/Feb not yet included)
+
+
+rm(effort, effort_raw, band, band_raw)
+
+
+## CAPTURE RATES----------
+
+# seasonal totals
+dat_sum = dat |> 
+  # drop incomplete seasons
+  filter(year > 1978) |> 
+  filter(!(year == 2025 & season == 'winter')) |> 
+  group_by(year, season) |> 
   summarize(n = sum(n),
-            nethours = sum(nethours)) %>% 
-  # calculate annual total
-  pivot_longer(n:nethours) %>% 
-  pivot_wider(names_from = season, values_from = value) %>% 
-  # drop 1978 (only data from Jan-Feb 1979 showing as winter 1978)
-  filter(!is.na(spring)) %>% 
-  mutate(total = fall + spring + winter) %>% 
-  pivot_longer(winter:total, names_to = 'season') %>% 
-  pivot_wider(names_from = name, values_from = value) %>% 
-  # calculate rate
+            nethours = sum(nethours),
+            .groups = 'drop')
+
+summary(dat_sum)
+hist(dat_sum$n) # 0 to 3000; peak 500-1000
+hist(dat_sum$nethours, breaks = 16) # few < 5000
+dat_sum |> filter(nethours < 5000)
+# fall 2025!
+
+# annual totals
+dat_annual = dat_sum |> 
+  # drop incomplete year (with incomplete winter 2025-26)
+  filter(year != 2025) |> 
+  group_by(year) |> 
+  summarize(season = 'total',
+            n = sum(n),
+            nethours = sum(nethours))
+
+# combine and calculate capture rate per 1000 net hours:
+dat_all = bind_rows(dat_sum |> filter(year > 1978), 
+                    dat_annual) |>
+  arrange(year, season) |> 
   mutate(rate = n / nethours * 1000,
          ratelab = format(round(rate, digits = 1), nsmall = 1),
          season = factor(season, levels = c('total', 'spring', 'fall', 'winter')),
          season = recode(season, total = 'Total',
                          spring = 'Spring/Summer (Mar - Jul)',
                          fall = 'Fall (Aug - Oct)',
-                         winter = 'Winter (Nov - Feb)')) %>% 
+                         winter = 'Winter (Nov - Feb)')) |> 
   select(year, season, rate, ratelab)
+
+summary(dat_all)
 
 
 # FIT SMOOTHED LINES ------------------------------------------------------
 
-g1 <- ggplot(dat, aes(year, rate, color = season)) +
-  geom_smooth(method="gam", method.args = list(family="quasipoisson"), 
+g1 <- ggplot(dat_all, aes(year, rate, color = season)) +
+  geom_smooth(method = "gam", 
+              method.args = list(family = "quasipoisson"), 
               formula = y ~ s(x), se = FALSE)
+g1
 
 # build plot object for rendering 
 gg1 <- ggplot_build(g1)
@@ -101,7 +176,7 @@ gg1 <- ggplot_build(g1)
 # extract data for the loess lines from the 'data' slot
 dat2 <- data.frame(x = gg1$data[[1]]$x,
                    smooth = gg1$data[[1]]$y,
-                   season = rep(levels(dat$season), each = 80)) %>% 
+                   season = rep(levels(dat_all$season), each = 80)) %>% 
   mutate(season = factor(season, levels = c('Total', 
                                             'Spring/Summer (Mar - Jul)', 
                                             'Fall (Aug - Oct)',
@@ -109,37 +184,29 @@ dat2 <- data.frame(x = gg1$data[[1]]$x,
 
 # PLOTLY ------------------------------------------------------------------
 
-# gg1 <- ggplot(dat, aes(year, rate, group = season, color = season)) + 
-#   geom_line(color = pointblue.palette[6]) +
-#   geom_point() +
-#   geom_smooth(method = 'gam', method.args = list(family = 'quasipoisson'),
-#               formula = y ~ s(x), se = FALSE) +
-#   labs(color = NULL) +
-#   theme_classic()
-
 # build interactive plot
 pal <- setNames(pointblue.palette[1:4], 
                 c("Winter (Nov - Feb)", "Spring/Summer (Mar - Jul)", 
                   "Fall (Aug - Oct)", "Total"))
 
-graph1 <- plot_ly() %>%
-  add_lines(data = dat2 %>% filter(season == 'Total'),
+graph1 <- plot_ly() |> 
+  add_lines(data = dat2 |> filter(season == 'Total'),
             x = ~x,
             y = ~smooth,
             color = ~season,
             colors = pal,
             legendgroup = ~season,
             hoverinfo = 'none',
-            line = list(width = 4)) %>% 
-  add_lines(data = dat2 %>% filter(season != 'Total'),
+            line = list(width = 4)) |> 
+  add_lines(data = dat2 |> filter(season != 'Total'),
             x = ~x,
             y = ~smooth,
             color = ~season,
             colors = pal,
             legendgroup = ~season,
             hoverinfo = 'none',
-            visible = 'legendonly') %>% 
-  add_markers(data = dat %>% filter(season == 'Total'),
+            visible = 'legendonly') |> 
+  add_markers(data = dat_all |> filter(season == 'Total'),
               x = ~year,
               y = ~rate,
               marker = list(size = 8),
@@ -147,8 +214,8 @@ graph1 <- plot_ly() %>%
               legendgroup = ~season,
               showlegend = FALSE,
               hoverinfo = 'x+text',
-              text = ~ratelab) %>%
-  add_markers(data = dat %>% filter(season != 'Total'),
+              text = ~ratelab) |> 
+  add_markers(data = dat_all |> filter(season != 'Total'),
             x = ~year,
             y = ~rate,
             marker = list(size = 8),
@@ -157,7 +224,7 @@ graph1 <- plot_ly() %>%
             showlegend = FALSE,
             hoverinfo = 'x+text',
             text = ~ratelab,
-            visible = 'legendonly') %>% 
+            visible = 'legendonly') |> 
   layout(yaxis = list(title = 'Capture rate (per 1000 net hours)',
                       font = list(size = 14),
                       showline = TRUE,
@@ -173,11 +240,15 @@ graph1 <- plot_ly() %>%
                       showgrid = FALSE),
          hovermode = 'x',
          legend = list(x = 1, xanchor = 'right', y = 1, yanchor = 'top'),
-         margin = list(r = 0, b = 10, t = 10)) %>%
+         margin = list(r = 0, b = 10, t = 10)) |> 
   config(displaylogo = FALSE, showTips = FALSE,
          modeBarButtonsToRemove = list('zoom2d', 'select2d', 'lasso2d',
                                        'zoomIn2d', 'zoomOut2d',
                                        'pan2d', 'toggleSpikelines'))
+
+# test in viewer pane:
+graph1
+
 
 graph1$dependencies <- c(graph1$dependencies,
                          list(
